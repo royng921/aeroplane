bb53bd7c134ab50561b625f5a0f81b0799fcc7a7
Royng921 <nyh@plkwcc.edu.hk>
2024-06-27 01:26:14 +0800

    勁到

--- src/components/UI/WasteManagement.tsx
+++ src/components/UI/WasteManagement.tsx
@@ -5,36 +5,34 @@ const WasteManagement = () => {
     <div className="mt-8 lg:mt-20 mx-auto max-w-[85%] xl:max-w-[100%] flex flex-col lg:flex-row lg:w-[80vw] justify-center text-white">
       <div className="flex-1 mb-4 xl:mr-3">
         <h1 className="text-2xl xl:text-[2.15rem] font-bold mb-2 xl:mb-5">
-          PROBLEM
+          我勁唔勁!!!!
         </h1>
         <p className="mb-4 xl:text-[1.5rem] max-w-[85%]">
-          Waste management is a big problem in our country. Most of the wastes
-          end up in landfills. This leads to many issues like:
+          勁到癲!!
         </p>
         <ul className="list-disc mb-4 ml-3 xl:text-[1.25rem]">
-          <li>Landfills growing, harming environment</li>
-          <li>Eutrophication due to waste accumulation</li>
-          <li>Consumption of toxic waste by animals</li>
-          <li>Leachate pollutes soil and water</li>
-          <li>Increase in toxins,threatening health</li>
-          <li>Land, water and air pollution</li>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+          <li>6</li>
         </ul>
       </div>
       <div className="flex-1 lg:mb-4 xl:ml-3">
         <h1 className="text-2xl font-bold mb-2 xl:text-[2.15rem] xl:mb-5">
-          APPROACH
+          西西
         </h1>
         <p className="mb-4 xl:text-[1.5rem]  max-w-[85%]">
-          To address these problems, an effective waste management approach
-          should be adopted. The approach can include:
+          CTO
         </p>
         <ul className="list-disc mb-4 ml-3 xl:text-[1.25rem]">
-          <li>Studied white papers on waste management</li>
-          <li>Analysed the components of household waste</li>
-          <li>Segregated into two classes (Organic and recyclable)</li>
-          <li>Automated the process by using IOT and machine learning</li>
-          <li>Reduce toxic waste ending in landfills</li>
-          <li>Implementing a waste reduction program</li>
+          <li>西西射波勁到</li>
+          <li>牙朗又射</li>
+          <li>牙葛又射</li>
+          <li>CTO都射</li>
+          <li>射到癲</li>
+          <li>射到傻</li>
         </ul>
       </div>
     </div>
